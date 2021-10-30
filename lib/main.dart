f0334fb364d1efbc5266546a0b8f5bb30d732d21
Nightmare <mengyanshou@gmail.com>
2021-10-30 21:45:53 +0800

    improve app~

--- lib/main.dart
+++ lib/main.dart
@@ -25,26 +25,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: true,
-      body: TerminalPage(),
+      home: const TerminalPage(),
     );
   }
 }
