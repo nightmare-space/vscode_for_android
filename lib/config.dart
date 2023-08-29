c2ced2e78b82e23d377f475aed000d58e70b4c53
nightmare <mengyanshou@gmail.com>
2023-08-29 19:43:47 +0800

    Commit some code

--- lib/config.dart
+++ lib/config.dart
@@ -1,19 +1,8 @@
-import 'http_handler.dart';
-
 class Config {
   Config._();
 
   /// 包名
   static const String packageName = 'com.nightmare.code';
-  static String dioScript = '''
-    touch $dioLockFile
-            echo -n dart_dio "\$@"
-            while [ -f $dioLockFile ]
-            do {
-              sleep 0.5
-            }
-            done
-            exit''';
 
   static String versionName = '1.2.1';
 }
