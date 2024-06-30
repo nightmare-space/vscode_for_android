aa57003ffe54d17b8357858097d660d76db89bbb
nightmare <mengyanshou@gmail.com>
2024-06-30 17:14:32 +0800

    Remove some code

--- android/app/src/main/java/com/nightmare/code/MainActivity.java
+++ android/app/src/main/java/com/nightmare/code/MainActivity.java
@@ -1,30 +1,11 @@
 package com.nightmare.code;
-
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
-
 import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
-
-import com.superad.ad_lib.ADInitListener;
-import com.superad.ad_lib.ADManage;
-import com.superad.ad_lib.SuperBannerAD;
-import com.superad.ad_lib.SuperFullUnifiedInterstitialAD;
-import com.superad.ad_lib.SuperFullUnifiedInterstitialADListener;
-import com.superad.ad_lib.SuperHalfUnifiedInterstitialAD;
-import com.superad.ad_lib.SuperHalfUnifiedInterstitialADListener;
-import com.superad.ad_lib.SuperSplashAD;
-import com.superad.ad_lib.SuperSplashADListener;
-import com.superad.ad_lib.SuperUnifiedBannerADListener;
-
 import io.flutter.embedding.android.FlutterFragment;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.embedding.engine.FlutterEngineCache;
@@ -95,186 +76,186 @@ public class MainActivity extends FragmentActivity {
     }
 
     public void showAppSplashAD() {
-        ViewGroup splash_container = findViewById(R.id.splash_container);
-        new SuperSplashAD(mContext, splash_container, new SuperSplashADListener() {
-            @Override
-            public void onError(Object error) {
-
-            }
-
-            @Override
-            public void onAdLoad() {
-
-            }
-
-            @Override
-            public void onADShow() {
-
-            }
-
-            @Override
-            public void onADClicked() {
-
-            }
-
-            @Override
-            public void onADDismissed() {
-                splash_container.removeAllViews();
-                splash_container.setVisibility(View.GONE);
-                // Toast.makeText(mContext, "开屏广告结束,请将程序跳转到主界面", Toast.LENGTH_SHORT).show();
-                fragmentManager
-                        .beginTransaction()
-                        .add(
-                                R.id.fl_container,
-                                flutterFragment,
-                                TAG_FLUTTER_FRAGMENT
-                        )
-                        .commit();
-            }
-
-            @Override
-            public void onAdTypeNotSupport() {
-
-            }
-
-        });
+//        ViewGroup splash_container = findViewById(R.id.splash_container);
+//        new SuperSplashAD(mContext, splash_container, new SuperSplashADListener() {
+//            @Override
+//            public void onError(Object error) {
+//
+//            }
+//
+//            @Override
+//            public void onAdLoad() {
+//
+//            }
+//
+//            @Override
+//            public void onADShow() {
+//
+//            }
+//
+//            @Override
+//            public void onADClicked() {
+//
+//            }
+//
+//            @Override
+//            public void onADDismissed() {
+//                splash_container.removeAllViews();
+//                splash_container.setVisibility(View.GONE);
+//                // Toast.makeText(mContext, "开屏广告结束,请将程序跳转到主界面", Toast.LENGTH_SHORT).show();
+//                fragmentManager
+//                        .beginTransaction()
+//                        .add(
+//                                R.id.fl_container,
+//                                flutterFragment,
+//                                TAG_FLUTTER_FRAGMENT
+//                        )
+//                        .commit();
+//            }
+//
+//            @Override
+//            public void onAdTypeNotSupport() {
+//
+//            }
+//
+//        });
     }
 
     public void showFullScreenDialog() {
-        SuperFullUnifiedInterstitialAD ad = new SuperFullUnifiedInterstitialAD((Activity) mContext, new SuperFullUnifiedInterstitialADListener() {
-
-            @Override
-            public void onError(Object error) {
-                Log.e("~~~~", "onError");
-            }
-
-            @Override
-            public void onAdLoad() {
-                Log.e("~~~~", "onAdLoad");
-            }
-
-            @Override
-            public void onAdClicked() {
-                Log.e("~~~~", "onAdClicked");
-            }
-
-            @Override
-            public void onAdShow() {
-                Log.e("~~~~", "onAdShow");
-            }
-
-            @Override
-            public void onADClosed() {
-                Log.e("~~~~", "onADClosed");
-
-            }
-
-            @Override
-            public void onRenderSuccess() {
-                Log.e("~~~~", "onRenderSuccess");
-
-            }
-
-            @Override
-            public void onRenderFail() {
-                Log.e("~~~~", "onRenderFail");
-            }
-
-            @Override
-            public void onAdTypeNotSupport() {
-                Log.e("~~~~", "onAdTypeNotSupport");
-            }
-        });
+//        SuperFullUnifiedInterstitialAD ad = new SuperFullUnifiedInterstitialAD((Activity) mContext, new SuperFullUnifiedInterstitialADListener() {
+//
+//            @Override
+//            public void onError(Object error) {
+//                Log.e("~~~~", "onError");
+//            }
+//
+//            @Override
+//            public void onAdLoad() {
+//                Log.e("~~~~", "onAdLoad");
+//            }
+//
+//            @Override
+//            public void onAdClicked() {
+//                Log.e("~~~~", "onAdClicked");
+//            }
+//
+//            @Override
+//            public void onAdShow() {
+//                Log.e("~~~~", "onAdShow");
+//            }
+//
+//            @Override
+//            public void onADClosed() {
+//                Log.e("~~~~", "onADClosed");
+//
+//            }
+//
+//            @Override
+//            public void onRenderSuccess() {
+//                Log.e("~~~~", "onRenderSuccess");
+//
+//            }
+//
+//            @Override
+//            public void onRenderFail() {
+//                Log.e("~~~~", "onRenderFail");
+//            }
+//
+//            @Override
+//            public void onAdTypeNotSupport() {
+//                Log.e("~~~~", "onAdTypeNotSupport");
+//            }
+//        });
     }
 
     public void showHalfDialog() {
-        SuperHalfUnifiedInterstitialAD ad = new SuperHalfUnifiedInterstitialAD((Activity) mContext, new SuperHalfUnifiedInterstitialADListener() {
-            @Override
-            public void onError(Object error) {
-                Log.e("~~~~~", "onError" + error.toString());
-            }
-
-            @Override
-            public void onAdLoad() {
-                Log.e("~~~~~", "onAdLoad");
-            }
-
-            @Override
-            public void onAdClicked() {
-                Log.e("~~~~~", "onAdClicked");
-            }
-
-            @Override
-            public void onAdShow() {
-                Log.e("~~~~~", "onAdShow");
-            }
-
-            @Override
-            public void onADClosed() {
-                Log.e("~~~~~", "onADClosed");
-            }
-
-            @Override
-            public void onRenderSuccess() {
-                Log.e("~~~~~", "onRenderSuccess");
-            }
-
-            @Override
-            public void onRenderFail() {
-                Log.e("~~~~~", "onRenderFail");
-            }
-
-            @Override
-            public void onAdTypeNotSupport() {
-
-            }
-        });
+//        SuperHalfUnifiedInterstitialAD ad = new SuperHalfUnifiedInterstitialAD((Activity) mContext, new SuperHalfUnifiedInterstitialADListener() {
+//            @Override
+//            public void onError(Object error) {
+//                Log.e("~~~~~", "onError" + error.toString());
+//            }
+//
+//            @Override
+//            public void onAdLoad() {
+//                Log.e("~~~~~", "onAdLoad");
+//            }
+//
+//            @Override
+//            public void onAdClicked() {
+//                Log.e("~~~~~", "onAdClicked");
+//            }
+//
+//            @Override
+//            public void onAdShow() {
+//                Log.e("~~~~~", "onAdShow");
+//            }
+//
+//            @Override
+//            public void onADClosed() {
+//                Log.e("~~~~~", "onADClosed");
+//            }
+//
+//            @Override
+//            public void onRenderSuccess() {
+//                Log.e("~~~~~", "onRenderSuccess");
+//            }
+//
+//            @Override
+//            public void onRenderFail() {
+//                Log.e("~~~~~", "onRenderFail");
+//            }
+//
+//            @Override
+//            public void onAdTypeNotSupport() {
+//
+//            }
+//        });
     }
 
     public void showBannerAD() {
         ViewGroup banner_container = findViewById(R.id.fl_container);
-        SuperBannerAD ad = new SuperBannerAD((Activity) mContext, banner_container, new SuperUnifiedBannerADListener() {
-            @Override
-            public void onError(Object var1) {
-                Log.e("~~~~", "onError");
-            }
-
-            @Override
-            public void onADLoad() {
-                Log.e("~~~~", "onADLoad");
-            }
-
-            @Override
-            public void onADShow() {
-                Log.e("~~~~", "onADShow");
-            }
-
-            @Override
-            public void onADClick() {
-                Log.e("~~~~", "onADClick");
-            }
-
-            @Override
-            public void onADClose() {
-                Log.e("~~~~", "onADClose");
-                banner_container.removeAllViews();
-            }
-
-            @Override
-            public void onRenderFail() {
-                Log.e("~~~~", "onRenderFail");
-            }
-
-            @Override
-            public void onRenderSuccess() {
-                Log.e("~~~~", "onRenderSuccess");
-            }
-
-            @Override
-            public void onAdTypeNotSupport() {
-
-            }
-        });
+//        SuperBannerAD ad = new SuperBannerAD((Activity) mContext, banner_container, new SuperUnifiedBannerADListener() {
+//            @Override
+//            public void onError(Object var1) {
+//                Log.e("~~~~", "onError");
+//            }
+//
+//            @Override
+//            public void onADLoad() {
+//                Log.e("~~~~", "onADLoad");
+//            }
+//
+//            @Override
+//            public void onADShow() {
+//                Log.e("~~~~", "onADShow");
+//            }
+//
+//            @Override
+//            public void onADClick() {
+//                Log.e("~~~~", "onADClick");
+//            }
+//
+//            @Override
+//            public void onADClose() {
+//                Log.e("~~~~", "onADClose");
+//                banner_container.removeAllViews();
+//            }
+//
+//            @Override
+//            public void onRenderFail() {
+//                Log.e("~~~~", "onRenderFail");
+//            }
+//
+//            @Override
+//            public void onRenderSuccess() {
+//                Log.e("~~~~", "onRenderSuccess");
+//            }
+//
+//            @Override
+//            public void onAdTypeNotSupport() {
+//
+//            }
+//        });
     }
 
     @Override
