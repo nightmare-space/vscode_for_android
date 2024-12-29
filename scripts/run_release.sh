e9fed4055fc479db78ec2728c0fe7795ea445209
nightmare <mengyanshou@gmail.com>
2024-12-30 02:20:19 +0800

    chore: Update

---
+++ scripts/run_release.sh
@@ -0,0 +1,1 @@
+flutter run --release --dart-define=CSVERSION=4.96.2
--- scripts/upload/upload.sh
+++
@@ -1,8 +0,0 @@
-LOCAL_DIR=$(
-    cd $(dirname $0)
-    pwd
-)
-PROJECT_DIR=$LOCAL_DIR/../..
-source $LOCAL_DIR/../properties.sh
-echo $PROJECT_DIR
-rsync -v $PROJECT_DIR/build/app/outputs/flutter-apk/app-release.apk $TARGET_PATH/$APP_NAME'_'$VERSION'_'Android_arm64.apk
